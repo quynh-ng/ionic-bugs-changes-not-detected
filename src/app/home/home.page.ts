7d57709615daa3b652f81f53bb6307a27bff9710
Quynh Nguyen <mr.quynh@gmail.com>
2018-09-25 22:54:37 +0700

    Reproduct the bug with color is empty string

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,8 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { Content } from '@ionic/angular';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss']
+	selector: 'app-home',
+	templateUrl: './home.page.html',
+	styleUrls: ['./home.page.scss']
 })
-export class HomePage {}
+export class HomePage {
+	constructor (
+	) {
+	}
+
+	login() {
+	}
+
+}
